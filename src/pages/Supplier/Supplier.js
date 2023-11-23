fe767fcdc51eae85cab921c9c6e7e89a40b8fec9
Andreza Leal <andrezaleal12@.com>
2023-11-23 05:57:20 -0300

    conexao com firebase fonecedores por id

--- src/pages/Supplier/Supplier.js
+++ src/pages/Supplier/Supplier.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import { db } from '../../services/firebaseConnections';
+import { doc, getDoc } from 'firebase/firestore';
 
 const SupplierPage = () => {
     const { id } = useParams();
+    const [supplier, setSupplier] = useState(true);
+
+    async function SuppierId(id) {
+        try {
+          const supplierDoc = await getDoc(doc(db, 'fornecedores', id));
+          if (supplierDoc.exists()) {
+            const supplierData = supplierDoc.data();
+            const lista = {
+              id: id,
+              nome: supplierData.nome,
+              categoria: supplierData.categoria,
+              descricao: supplierData.descricao,
+            };
+            console.log(lista)
+            setSupplier(lista);
+          } else {
+            console.log("Planta Não encontrada");
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      useEffect(() => {
+        SuppierId(id);
+      }, [id]);
+      console.log(supplier);
     return (
         <div>
              <div className="header container flex mt-8 justify-between items-center">
-                <h1 className="title font-bold text-3xl align-middle"> &lt; Embalagens </h1>
+                <h1 className="title font-bold text-3xl align-middle"> &lt; Fornecedor </h1>
                 <div className="profile-div w-14 h-14 rounded-full bg-white">
                 </div>
             </div>

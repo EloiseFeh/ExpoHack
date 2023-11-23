dbc7e2e5c8a7580b46fc9a2929e22fa0a8d1cb0b
Andreza Leal <andrezaleal12@.com>
2023-11-23 03:07:50 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css"
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
 import Requests from "./pages/Requests";
+import Cadastro from "./pages/Cadastro";
 import Listings from "./pages/Listings";
+import AuthProvider from "./services/auth";
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/requests" element={<Requests/>} />
-                <Route path="/listings" element={<Listings/>} />
-            </Routes>
-        </Router>
+        
+            <Router>
+                <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/requests" element={<Requests />} />
+                    <Route path="/cadastro" element={<Cadastro />} />
+                    <Route path="/listings" element={<Listings />} />
+                </Routes>
+                </AuthProvider>
+            </Router>
     );
 };
 

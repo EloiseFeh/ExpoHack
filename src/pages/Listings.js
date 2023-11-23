3759b7e345623eebbcc9b7bd050e09fbbe39736e
EloiseFeh <eloiseferreira045@gmail.com>
2023-11-22 23:23:00 -0300

    setup router

---
+++ src/pages/Listings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Listings = () => {
+    return (
+        <div>
+        <h1>Listings</h1>
+        <p>This is the listings page</p>
+        </div>
+    );
+}
+
+export default Listings;
---
+++ src/pages/Requests.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Requests = () => {
+    return (
+        <div>
+        <h1>Requests</h1>
+        <p>This is the requests page</p>
+        </div>
+    );
+}
+
+export default Requests;
